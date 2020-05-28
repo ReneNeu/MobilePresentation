f71577dcea7e6d2c27652647411bc386431534d1
ReneNeu <reneneuhaus.RN@googlemail.com>
2020-05-28 13:52:44 +0200

    Add Second sensor

--- app/src/main/java/com/example/mobileambienttemperature/MainActivity.kt
+++ app/src/main/java/com/example/mobileambienttemperature/MainActivity.kt
@@ -7,21 +7,31 @@ import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() , SensorEventListener {
     private lateinit var sensorManager: SensorManager
-    private var pressure: Sensor? = null
+
+    private var temperatureSensor: Sensor? = null
+    private var light: Sensor? = null
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         // Get an instance of the sensor service, and use that to get an instance of
         // a particular sensor.
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
-        pressure = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+
+
+        temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+
+
+        //image.animate().rotation(rotationY).alpha(0.5F).setDuration(0)
     }
 
     override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
@@ -32,13 +42,21 @@ class MainActivity : AppCompatActivity() , SensorEventListener {
         if (event.sensor.type == Sensor.TYPE_AMBIENT_TEMPERATURE) {
             val ambient_temperature = event.values[0]
             findViewById<TextView>(R.id.temperature_output).text = ambient_temperature.toString() + " °C"
+        } else if (event.sensor.type == Sensor.TYPE_LIGHT) {
+            val lightValue = event.values[0]
+            val image = findViewById<ImageView>(R.id.imageView3)
+            findViewById<TextView>(R.id.output_rotation).text = lightValue.toString()
+            var test = lightValue / 4
+            test /= 10000
+            image.animate().alpha(test).setDuration(0)
         }
     }
 
     override fun onResume() {
         // Register a listener for the sensor.
         super.onResume()
-        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, temperatureSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
     override fun onPause() {
@@ -47,3 +65,6 @@ class MainActivity : AppCompatActivity() , SensorEventListener {
         sensorManager.unregisterListener(this)
     }
 }
+
+
+
